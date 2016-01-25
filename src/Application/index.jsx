89d2670ba4233dcaf4c11a954b22a73049986cec
Sam Roberts <samgqroberts@gmail.com>
2016-01-24 21:37:54 -0500

    add bootstrap through sass, apply to app skeleton

--- src/Application/index.jsx
+++ src/Application/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 const cssClasses = {
   comp: 'application',
@@ -7,14 +8,36 @@ const cssClasses = {
 };
 
 const Application = React.createClass({
+  renderMenu() {
+    const menuClassNames = classNames({
+      [cssClasses.menu]: true,
+      'col-md-3': true,
+    });
+    return (
+      <div className={menuClassNames}>
+        hi Im the menu
+      </div>
+    );
+  },
+
+  renderContent() {
+    const contentClassNames = classNames({
+      [cssClasses.content]: true,
+      'col-md-9': true,
+    });
+    return (
+      <div className={contentClassNames}>
+        hi Im the content
+      </div>
+    );
+  },
+
   render() {
     return (
-      <div className={cssClasses.comp}>
-        <div className={cssClasses.menu}>
-          I am a menu section
-        </div>
-        <div className={cssClasses.content}>
-          I am a content section
+      <div className={cssClasses.comp + ' container'}>
+        <div className="row">
+          {this.renderMenu()}
+          {this.renderContent()}
         </div>
       </div>
     );
