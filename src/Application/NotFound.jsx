3e4d2a427ea762e06e93bd3998bb3bfd7d359bdc
Sam Roberts <samgqroberts@github.com>
2016-02-14 19:19:47 -0500

    update NotFound to be user-facing appropriate

--- src/Application/NotFound.jsx
+++ src/Application/NotFound.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 
-const cssClasses = {
-  comp: 'not-found-comp',
-};
-
 const NotFound = React.createClass({
   render() {
     return (
-      <div className={cssClasses.comp}>
-        I am the Not Found content component.
+      <div>
+        <h2>
+          Page not found!
+        </h2>
       </div>
     );
   },
